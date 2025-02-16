305bea4d60439ecd1a3f46d16510757bc610a572
Andrey Shilkov <a.shilkov@3divi.com>
2025-02-16 21:20:39 +0500

    Added more helper functions to utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,6 @@
 import os
-from typing import List
+import json
+from typing import List, Dict
 from pydantic import FilePath, DirectoryPath
 from pathlib import Path
 
@@ -11,3 +12,14 @@ def traverse_directory_for_files(root_folder: DirectoryPath) -> List[FilePath]:
             file_path: FilePath = Path(dirpath) / file_name
             output_files.append(file_path)
     return output_files
+
+
+def write_json(data: Dict, json_file: Path) -> None:
+    with open(json_file, "w") as json_file:
+        json.dump(data, json_file, indent=2)
+
+
+def read_json(json_file: FilePath) -> Dict:
+    with open(json_file, "r") as json_file:
+        data = json.load(json_file)
+    return data

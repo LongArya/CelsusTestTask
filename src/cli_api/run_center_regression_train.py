57dc0a62ab6c8be70ba98aa031e48517e97b98ec
Andrey Shilkov <a.shilkov@3divi.com>
2025-02-18 00:35:00 +0500

    script for running center regression train

---
+++ src/cli_api/run_center_regression_train.py
@@ -0,0 +1,21 @@
+import click
+from ..train.center_regressor_train import run_train
+from ..data.dataset import CenterRegressionDatasetReader
+from ..schemas.config.train_config import TrainConfig
+from ..utils import read_yaml
+from ..consts import TRAIN_DATASET_ROOT, VAL_DATASET_ROOT
+from pathlib import Path
+
+
+@click.command()
+@click.option("--config_file", type=Path)
+def main(config_file: Path):
+    config_data = read_yaml(config_file)
+    config = TrainConfig.model_validate(config_data)
+    train_ds = CenterRegressionDatasetReader(TRAIN_DATASET_ROOT)
+    val_ds = CenterRegressionDatasetReader(VAL_DATASET_ROOT)
+    run_train(train_dataset=train_ds, val_dataset=val_ds, config=config)
+
+
+if __name__ == "__main__":
+    pass

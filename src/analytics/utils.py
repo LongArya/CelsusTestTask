ed127c3b634f59a67eeed75b01d7615b4d53a142
Andrey Shilkov <a.shilkov@3divi.com>
2025-02-19 23:38:53 +0500

    Fixed interaction with extra PR curve point

--- src/analytics/utils.py
+++ src/analytics/utils.py
@@ -6,7 +6,16 @@ from scipy.stats import hmean
 def get_best_f1_score_point(
     precision: np.ndarray, recall: np.ndarray, thresholds: np.ndarray
 ) -> PRCurvePoint:
-    f1_scores = hmean(np.vstack((precision, recall)), axis=0)
+    f1_scores: np.ndarray
+    if len(precision) != len(recall):
+        raise ValueError("Precision and recall should have same number of elements")
+    if len(precision) - len(thresholds) > 1:
+        raise ValueError("Precision and recall is allowed to have only one extra point")
+
+    if len(precision) > len(thresholds):
+        f1_scores = hmean(np.vstack((precision[:-1], recall[:-1])), axis=0)
+    else:
+        f1_scores = hmean(np.vstack((precision, recall)), axis=0)
     max_f1_index = np.argsort(f1_scores)[-1]
     best_point = PRCurvePoint(
         prcn=precision[max_f1_index],
